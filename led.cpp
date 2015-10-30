24e8475dfa6923088bd8c236ea14fefa6c54d361
Jeremy Stanley <jeremy@instructure.com>
2015-10-30 16:28:52 -0600

    look for unused DMA channels

--- led.cpp
+++ led.cpp
@@ -1,9 +1,10 @@
 // GPIO  9 = red
 // GPIO 10 = blue
 // GPIO 11 = green
-// brightness levels 0..511 (because reasons. 9 bits is better than 8, right?)
-// TODO FIXME hack pwm.c to make it possible to use one channel
-// TODO FIXME SRS FRD actually pick a DMA channel that isn't in use
+// TODO FIXME hack this to use one DMA channel
+
+#include <stdio.h>
+#include <stdlib.h>
 
 extern "C" {
 #include "pwm/pwm.h"
@@ -15,9 +16,10 @@ LED::LED()
 {
     set_loglevel(LOG_LEVEL_ERRORS);
     setup(PULSE_WIDTH_INCREMENT_GRANULARITY_US_DEFAULT, DELAY_VIA_PCM);
-    init_channel(12, 5120);
-    init_channel(13, 5120);
-    init_channel(14, 5120);
+    allocate_dma_channels();
+    init_channel(m_dma_channels[0], 5120);
+    init_channel(m_dma_channels[1], 5120);
+    init_channel(m_dma_channels[2], 5120);
 }
 
 LED::~LED()
@@ -25,9 +27,38 @@ LED::~LED()
     shutdown();
 }
 
+void LED::allocate_dma_channels()
+{
+    FILE *s = popen("cat /sys/module/dma/parameters/dmachans", "r");
+    if (s) {
+        int bitmask = -1;
+        fscanf(s, "%d", &bitmask);
+        pclose(s);
+        if (bitmask == -1) {
+            fprintf(stderr, "LED: failed to read list of DMA channels\n");
+            exit(1);
+        }
+        int channels = 0;
+        for(int ch = 14; ch >= 0; --ch) {
+            if (0 == (bitmask & (1 << ch))) {
+                m_dma_channels[channels++] = ch;
+                if (channels == 3)
+                    break;
+            }
+        }
+        if (channels < 3) {
+            fprintf(stderr, "LED: insufficient available DMA channels");
+            exit(1);
+        }
+    } else {
+        perror("LED: failed to get available DMA channels");
+        exit(1);
+    }
+}
+
 void LED::set_color(int r, int g, int b)
 {
-    add_channel_pulse(12, 9,  0, r);
-    add_channel_pulse(13, 10, 0, b);
-    add_channel_pulse(14, 11, 0, g);
+    add_channel_pulse(m_dma_channels[0],  9, 0, r);
+    add_channel_pulse(m_dma_channels[1], 10, 0, b);
+    add_channel_pulse(m_dma_channels[2], 11, 0, g);
 }
--- led.h
+++ led.h
@@ -6,5 +6,11 @@ public:
     LED();
     ~LED();
     
+    // brightness range is 0..511, because reasons.
+    // 9 bits are better than 8, right? 
     void set_color(int r, int g, int b);
+
+private:
+    int m_dma_channels[3];
+    void allocate_dma_channels();
 };
