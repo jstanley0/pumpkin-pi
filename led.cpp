21a0f009097e4215c6807189c9c5561648086325
Jeremy Stanley <jstanley0@gmail.com>
2015-10-30 14:49:48 +0000

    it's aliiiiiive

--- led.cpp
+++ led.cpp
@@ -1,3 +1,10 @@
+// GPIO  9 = red
+// GPIO 10 = blue
+// GPIO 11 = green
+// brightness levels 0..511 (because reasons. 9 bits is better than 8, right?)
+// TODO FIXME hack pwm.c to make it possible to use one channel
+// TODO FIXME SRS FRD actually pick a DMA channel that isn't in use
+
 extern "C" {
 #include "pwm/pwm.h"
 }
@@ -8,7 +15,9 @@ LED::LED()
 {
     set_loglevel(LOG_LEVEL_ERRORS);
     setup(PULSE_WIDTH_INCREMENT_GRANULARITY_US_DEFAULT, DELAY_VIA_PCM);
-    init_channel(0, 5120);
+    init_channel(12, 5120);
+    init_channel(13, 5120);
+    init_channel(14, 5120);
 }
 
 LED::~LED()
@@ -18,8 +27,7 @@ LED::~LED()
 
 void LED::set_color(int r, int g, int b)
 {
-    clear_channel(0);
-    if (r) add_channel_pulse(0, 9,  0, r);
-    if (b) add_channel_pulse(0, 10, 0, b);
-    if (g) add_channel_pulse(0, 11, 0, g);
+    add_channel_pulse(12, 9,  0, r);
+    add_channel_pulse(13, 10, 0, b);
+    add_channel_pulse(14, 11, 0, g);
 }
