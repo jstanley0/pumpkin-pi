5620a5557aa5c067e6c7e89f6a2839caf5d33e27
Jeremy Stanley <jstanley0@gmail.com>
2017-10-26 20:14:00 +0000

    add some stuff to the web interface

--- web_service.rb
+++ web_service.rb
@@ -10,5 +10,31 @@ get '/' do
 end
 
 post '/say' do
-    $pi.say params[:text]
+    channel = params[:channel] || 's'
+    if params[:color]
+        $pi.color 'loud', channel, params[:color], params[:color2]    
+    end
+    $pi.say params[:text], channel, params[:voice]
 end
+
+post '/play' do
+    channel = params[:channel] || 's'
+    if params[:color]
+        $pi.color 'loud', channel, params[:color], params[:color2]    
+    end
+    $pi.play params[:sound], channel
+end
+
+post '/candle' do
+    $pi.candle([1, '1', 'yes', 'on', 'true'].include?(params[:status]) ? 'on' : 'off')
+end
+
+post '/colors' do
+    channel = params[:channel] || 's'
+    $pi.color 'dark', channel, params[:dark], params[:dark2] if params[:dark]
+    $pi.color 'bright', channel, params[:bright], params[:bright2] if params[:bright]
+    $pi.color 'silent', channel, params[:silent], params[:silent2] if params[:silent]
+    $pi.color 'loud', channel, params[:loud], params[:loud2] if params[:loud]
+    'colors updated'
+end
+
